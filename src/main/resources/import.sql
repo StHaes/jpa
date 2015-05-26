5ece6c21039fa390657abf99d6bb244f198d8d75
Stijn Haesendonck <st.haesendonck@gmail.com>
2015-05-26 11:22:32 +0200

    Added Repo and Books

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into book(title, author, isbn) values('Nineteen Eighty Four', 'George Orwell', '0000000001');
+insert into book(title, author, isbn) values('Alice''s Adventures In Wonderland', 'Lewis Carroll', '0000000002');
