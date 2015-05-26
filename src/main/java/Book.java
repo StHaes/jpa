be71ed6b60d1e18cde6d122b8ffb39af9b1aa5c0
Stijn Haesendonck <st.haesendonck@gmail.com>
2015-05-26 11:56:12 +0200

    added unitest and main method

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,5 +1,6 @@
-import org.hibernate.annotations.Entity;
 
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
